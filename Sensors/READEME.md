8c6efa067e597fc87608b27a8e1c0a54da89f50d
JinwooSeong <brianseong.dev@gmail.com>
2023-02-06 15:50:28 -0800

    feat: finished the FSR and MPU6050 implementation, need to add bluetooth module to the code and test with iphone

--- Sensors/READEME.md
+++ Sensors/READEME.md
@@ -6,3 +6,8 @@
 - [ESP32 + Accelerometer M6050](https://www.tutorialspoint.com/esp32_for_iot/interfacing_esp32_with_mpu6050.htm)
 - [ESP32 + FSR(Force Sensing Resistor)](https://lastminuteengineers.com/fsr-arduino-tutorial/)
 - [How to connect ESP32 with ADS1115 analog extention](https://microcontrollerslab.com/ads1115-external-adc-with-esp32/)
+- [How to setup HC05 on arduino to connect to iPhone](https://www.youtube.com/watch?v=NXlyo0goBrU)
+- [Connect HM10 to iphone](https://www.hangar42.nl/hm10)
+- [ESP8266 with accelerometer](https://randomnerdtutorials.com/esp8266-nodemcu-mpu-6050-accelerometer-gyroscope-arduino/)
+- [Using Multiple I2C devices on ESP32](https://randomnerdtutorials.com/esp32-i2c-communication-arduino-ide/)
+- [ESP8266 ESP32 I2C Scanner](https://github.com/jainrk/i2c_port_address_scanner/blob/master/i2c_port_address_scanner/i2c_port_address_scanner.ino)
--- Sensors/shoes_sensors_code/shoes_sensors_code.ino
+++ Sensors/shoes_sensors_code/shoes_sensors_code.ino
@@ -1,52 +1,63 @@
 #include <Adafruit_ADS1X15.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 #define I2C_SDA 17
 #define I2C_SCL 21
+#define IMU_SDA 26
+#define IMU_SCL 18
 
-
-TwoWire I2CADS = TwoWire(0);
+// TwoWire I2CADS = TwoWire(0);
+// TwoWire I2CIMU = TwoWire(1);
 Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 
 const int fsr4Pin = 34;
 
-// double voltage2kg(double totalVoltage, double fsrVoltage) {
-//   if (fsrVoltage == 0) {
-//     return 0.0;
-//   }
-//   double fsrResistance = totalVoltage - fsrVoltage;
-//   fsrResistance *= 10000;
-//   fsrResistance /= fsrVoltage;
-//   // double fsrResistance = 10000 * fsrVoltage / (totalVoltage - fsrVoltage);
-//   // double fsrResistance = (-3300) * 10000 / fsrVoltage;
-//   Serial.print("resistance: "); Serial.println(fsrResistance);
-  
-//   double fsrConductance = 1000000 / fsrResistance;
-//   Serial.print("conductance: "); Serial.println(fsrConductance);
+Adafruit_MPU6050 mpu;
 
-//   double fsrForce;
-//   if (fsrConductance <= 100) {
-//     fsrForce = fsrConductance / 25;
-//   } 
-//   // else {
-//   //   fsrForce = fsrConductance - 1000;
-//   //   fsrForce /= 30;
-//   // }
-  
-//   return fsrForce;
-// }
+// const uint16_t MPU_ADDR = 0x68; // I2C address of the MPU-6050
+// int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 
 void setup(void)
 {
   Serial.begin(115200);
   
   pinMode(fsr4Pin, INPUT);
+  Serial.println("here");
 
-  I2CADS.begin(I2C_SDA, I2C_SCL, 100000);
-  if (!ads.begin(0x48, &I2CADS)) {
+  Wire.begin(I2C_SDA, I2C_SCL, 1000000);
+  // Wire1.begin(IMU_SDA, IMU_SCL, 1000000);
+  // I2CADS.begin(I2C_SDA, I2C_SCL, 1000000);
+  if (!ads.begin(0x48, &Wire)) {
     Serial.println("Failed to initialize ADS.");
     while (1);
   }
+
+  Serial.println("here2");
+
+  // I2CIMU.begin(IMU_SDA, IMU_SCL, 1000000);
+  Serial.println("here3");
+  if (!mpu.begin(0x68, &Wire)) {
+    Serial.println("Sensor init failed");
+    while (1)
+      yield();
+  }
+  Serial.println("MPU6050 Found!");
+
+  //setupt motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(1);
+  mpu.setMotionDetectionDuration(20);
+  mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+
+  // Wire.begin(IMU_SDA, IMU_SCL, 100000); // sda, scl, clock speed
+  // Wire.beginTransmission(MPU_ADDR);
+  // Wire.write(0x6B);  // PWR_MGMT_1 register
+  // Wire.write(0);     // set to zero (wakes up the MPU−6050)
+  // Wire.endTransmission(true);
 }
 
 void loop(void)
@@ -77,6 +88,20 @@ void loop(void)
   Serial.print("3: "); Serial.print(adc3); Serial.print("  "); Serial.print(adc3 * (current_voltage / 32767.0)); Serial.println("V");
   Serial.print("4: "); Serial.print(fsr4Read); Serial.print("  "); Serial.print(fsr4voltage); Serial.println("V");
   Serial.println();
+  
+  if(mpu.getMotionInterruptStatus()) {
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    /* Print out the values */
+    Serial.print("AccelX:"); Serial.print(a.acceleration.x); Serial.print(","); 
+    Serial.print("AccelY:"); Serial.print(a.acceleration.y); Serial.print(",");
+    Serial.print("AccelZ:"); Serial.print(a.acceleration.z); Serial.print(", ");
+    Serial.print("GyroX:"); Serial.print(g.gyro.x); Serial.print(",");
+    Serial.print("GyroY:"); Serial.print(g.gyro.y); Serial.print(",");
+    Serial.print("GyroZ:"); Serial.print(g.gyro.z); Serial.println("");
+  }
 
   delay(1000);
 }
