384000dda3826a4799e0a4a8001cac760bcc4689
JinwooSeong <brianseong.dev@gmail.com>
2023-02-13 14:50:53 -0800

    graph working in web frontend

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Line } from "react-chartjs-2";
+import "chartjs-plugin-streaming";
+import "./styles.css";
+import moment from "moment";
+
+const Chart = require("react-chartjs-2").Chart;
+
+const chartColors = {
+  red: "rgb(255, 99, 132)",
+  orange: "rgb(255, 159, 64)",
+  yellow: "rgb(255, 205, 86)",
+  green: "rgb(75, 192, 192)",
+  blue: "rgb(54, 162, 235)",
+  purple: "rgb(153, 102, 255)",
+  grey: "rgb(201, 203, 207)"
+};
+
+const color = Chart.helpers.color;
+const data = {
+  datasets: [
+    {
+      label: "Dataset 1 (linear interpolation)",
+      backgroundColor: color(chartColors.red)
+        .alpha(0.5)
+        .rgbString(),
+      borderColor: chartColors.red,
+      fill: false,
+      lineTension: 0,
+      borderDash: [8, 4],
+      data: []
+    }
+  ]
+};
+
+const options = {
+  elements: {
+    line: {
+      tension: 0.5
+    }
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "realtime",
+        distribution: "linear",
+        realtime: {
+          onRefresh: function(chart) {
+            chart.data.datasets[0].data.push({
+              x: moment(),
+              y: Math.random()
+            });
+          },
+          delay: 3000,
+          time: {
+            displayFormat: "h:mm"
+          }
+        },
+        ticks: {
+          displayFormats: 1,
+          maxRotation: 0,
+          minRotation: 0,
+          stepSize: 1,
+          maxTicksLimit: 30,
+          minUnit: "second",
+          source: "auto",
+          autoSkip: true,
+          callback: function(value) {
+            return moment(value, "HH:mm:ss").format("mm:ss");
+          }
+        }
+      }
+    ],
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+          max: 1
+        }
+      }
+    ]
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Line data={data} options={options} />
     </div>
   );
 }
