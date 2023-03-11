e81781b730967ee3b32d1a58a18c9de22e99beae
JinwooSeong <brianseong.dev@gmail.com>
2023-03-10 17:59:19 -0800

    feat: updating pcb board

---
+++ backend/dataprocess/dataprocessing.py
@@ -0,0 +1,7 @@
+
+def call_antyush_function():
+    # current_time
+    # last_call_time
+    # get the data during this time period
+    # calcuate the math
+    # return the data ( array of steps)
